76024bd634900e83686bcd52ab73b2af93ec2a41
abraham <4braham@gmail.com>
2011-03-09 08:23:01 -0800

    removed some error handling

--- server.js
+++ server.js
@@ -38,10 +38,7 @@ http.createServer(function (request, result) {
     callback = options.query.callback;
   }
 
-  if (!opttions.pathname.length) {
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.end('{"error":"Unknown API method"}');
-  } else if (options.pathname.indexOf('/v1/') !== 0) {
+  if (options.pathname.indexOf('/v1/') !== 0) {
     res.writeHead(404, {'Content-Type': 'application/json'});
     res.end('{"error":"Unknown API version"}');
   } else if (options.pathname !== '/v1/intersect.json') {
